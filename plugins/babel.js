8fc387a7fd646a221d532b44b332806adacd1767
Hadeeb Farhan <hadeebfarhan1@gmail.com>
2020-01-21 18:25:36 +0530

    add babel react-jsx plugins

--- plugins/babel.js
+++ plugins/babel.js
@@ -1,5 +1,7 @@
 const getDefaultPreset = require("razzle/babel");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 function preset() {
   // Destructuring default preset so
   // babel-esm-plugin can detect preset-env
@@ -9,9 +11,12 @@ function preset() {
     plugins: [
       ...defaultpreset.plugins,
       require.resolve("babel-plugin-macros"),
-      require.resolve("babel-plugin-emotion")
+      require.resolve("babel-plugin-emotion"),
+      isProduction &&
+        require.resolve("@babel/plugin-transform-react-inline-elements"),
+      require.resolve("@babel/plugin-transform-react-constant-elements")
       // more plugins
-    ]
+    ].filter(Boolean)
   };
 }
 
